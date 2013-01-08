c56ee8f292364e71da16dc728cf0c3ceb738d3d0
Joon Min <joon.min@xnuvo.com>
2013-01-08 10:46:04 +0900

    Login from d1

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :nuvo, "5632dc5b2bff993a82050f842fce7f037d933311cade8ee566a656436720fe6c", "3b29c9719a407ea363c4af50e41d8f397b29809d444499b64f24498c4c349669",
     {
       :client_options => {
-        :site => 'http://dev3.xnuvo.com:3000',
+        :site => 'http://d1.xnuvo.com',
         :authorize_path => '/oauth/authorize'
       }
     }
