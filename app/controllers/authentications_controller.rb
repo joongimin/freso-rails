1d531257b4adc3f9753c046dbf1384c65a51c9f9
Joon Min <joon.min@xnuvo.com>
2013-01-13 03:08:20 +0900

    Fixed bug where it doesn't proceed to login when registering for the first time

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,4 +1,12 @@
 class AuthenticationsController < ApplicationController
+private
+  def login(user)
+    sign_in(:user, user)
+    @referer = params[:referer]
+    render :create, :layout => false
+  end
+
+public
   def index
     @authentications = current_user.authentications if current_user
   end
@@ -7,10 +15,7 @@ class AuthenticationsController < ApplicationController
     omniauth = request.env["omniauth.auth"]
     authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
     if authentication
-      sign_in(:user, authentication.user)
-      @referer = params[:referer]
-      render :create, :layout => false
-      #sign_in_and_redirect(:user, authentication.user)
+      login(authentication.user)
     elsif current_user
       current_user.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'])
       flash[:notice] = "Authentication successful."
@@ -19,8 +24,7 @@ class AuthenticationsController < ApplicationController
       user = User.new
       user.apply_omniauth(omniauth)
       if user.save
-        flash[:notice] = "Signed in successfully."
-        sign_in_and_redirect(:user, user)
+        login(user)
       else
         session[:omniauth] = omniauth.except('extra')
         redirect_to new_user_registration_url
