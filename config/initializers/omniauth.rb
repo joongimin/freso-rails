602bf9dcff167f7df2f38c7ba1fee58a75584577
Joon Min <joon.min@xnuvo.com>
2013-01-15 01:32:10 +0900

    Set production to login using d1.xnuvo.com

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -16,6 +16,12 @@ Rails.application.config.middleware.use OmniAuth::Builder do
         }
       }
   else
-    provider :nuvo, "26324ecdb8519234aca3bda832a7dd91c46c0bb5efc657b413e15af0c024e38e", "2de234dbefbb4bc69812ce2bcc6e6b36e8aa4a15f9b9de4461896594512d5e22"
+    provider :nuvo, "26324ecdb8519234aca3bda832a7dd91c46c0bb5efc657b413e15af0c024e38e", "2de234dbefbb4bc69812ce2bcc6e6b36e8aa4a15f9b9de4461896594512d5e22",
+      {
+        :client_options => {
+          :site => 'https://d1.xnuvo.com',
+          :authorize_path => '/oauth/authorize'
+        }
+      }
   end
 end
