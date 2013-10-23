2a5bea47ac478db96d7548a013d283a7299574cd
Joon Min <joon.min@the-nuvo.com>
2013-10-24 01:53:12 +0900

    Removed asset fro deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,16 +10,14 @@ set :normalize_asset_timestamps, false
 set :whenever_command, "bundle exec whenever"
 set :maintenance_template_path, File.expand_path("../recipes/templates/maintenance.html.erb", __FILE__)
 set :unicorn_workers, 16
-set :asset_host, "nm-as1.the-nuvo.com"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
-set :postgresql_database, "nuvo_mc"
-set :postgresql_host, "nm-db1.the-nuvo.com"
-set :assets_prefix, "production_assets"
-set :branch, "master"
-server "fre.so", :app, :web, :db, :primary => true
+set :postgresql_database, 'freso'
+set :postgresql_host, 'db1.fre.so'
+set :branch, 'master'
+server 'fre.so', :app, :web, :db, :primary => true
 
 require "bundler/capistrano"
 require "capistrano/maintenance"
