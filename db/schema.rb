75cf4f95432d322f95c253abf648f0f9119de064
Joon Min <joon.min@the-nuvo.com>
2013-11-15 12:36:23 +0900

    link hash key to has_key address

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131023112236) do
+ActiveRecord::Schema.define(version: 20131115033316) do
 
   create_table "routes", force: true do |t|
     t.string   "hash_key"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(version: 20131023112236) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "routes", ["hash_key"], name: "index_routes_on_hash_key", using: :btree
 
 end
