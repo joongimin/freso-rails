f628541e6b5bb24244bfb32322ce83d4db3eba70
Joon Min <joon.min@the-nuvo.com>
2014-01-20 21:18:39 +0900

    Simplified routes table

--- db/migrate/20131023112236_create_routes.rb
+++ db/migrate/20131023112236_create_routes.rb
@@ -1,10 +1,9 @@
 class CreateRoutes < ActiveRecord::Migration
   def change
     create_table :routes do |t|
-      t.string :hash_key
-      t.text :url
-
-      t.timestamps
+      t.string :hash_key, limit: 255, null: false
+      t.string :url, null: false
+      t.datetime :created_at, null: false
     end
   end
 end
