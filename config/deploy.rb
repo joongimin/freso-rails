510eb33f9b6883896412643742d24584656fe6e4
Joon Min <joon.min@the-nuvo.com>
2014-05-23 11:09:20 -0400

    rbenv_ruby 2.1.1 -> 2.1.2

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
 set :ssh_options, {forward_agent: true}
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system backups}
-set :rbenv_ruby, '2.1.1'
+set :rbenv_ruby, '2.1.2'
 set :unicorn_workers, 16
 set :format, :notification
 set :notify_from, 'Crema <crema@the-nuvo.com>'
